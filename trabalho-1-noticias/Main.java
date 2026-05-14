fa0529712ee0bb364c8f099c40f2b169f3f17709
mateusfoliveira <mateussp2005@gmail.com>
2026-05-14 19:00:29 -0300

    Refactor(Sistema): isola metodo main

---
+++ trabalho-1-noticias/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Sistema sistema = new Sistema();
+
+        sistema.menu(scanner);
+    }
+}
--- trabalho-1-noticias/Sistema.java
+++ trabalho-1-noticias/Sistema.java
@@ -116,13 +116,4 @@ public class Sistema {
 
         scanner.close();
     }
-
-    // inicia programa
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        Sistema sistema = new Sistema();
-
-        sistema.menu(scanner);
-    }
 }
